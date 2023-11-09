f1947bbd68d12ed6d02c3f05b5acf73c9ffdd5c8
Akunov4594 <frontdevkg@gmail.com>
2023-11-09 23:42:51 +0600

    done header

---
+++ scripts/cart.js
@@ -0,0 +1,34 @@
+
+const cart_order_list = [
+  {
+    image: "Набор макарон",
+    name: "",
+    price: 950,
+    discount: -30,
+    different_types: [
+      {
+        name:"Трюфель",
+        quantity: 2
+      },
+      {
+        name: "Розмарин",
+        quantity: 3
+      },
+      {
+        name: "Ананас",
+        quantity: 1
+      },
+      {
+        name: "Кокос",
+        quantity: 4
+      },
+    ],
+    count: 1
+  },
+  {
+    image: "Упаковка с цветами",
+    name: "",
+    price: 50,
+    count: 1
+  },
+]
